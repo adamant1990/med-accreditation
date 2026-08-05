e1e6e154b6c6299045c31dcb2c75b7929f4ad568
adamant1990 <161812142+adamant1990@users.noreply.github.com>
2026-08-05 22:04:53 +0500

    Add FAQ and complete premium page structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Services from "./components/Services";
 import Advantages from "./components/Advantages";
 import WorkProcess from "./components/WorkProcess";
 import Reviews from "./components/Reviews";
+import FAQ from "./components/FAQ";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
 
 import "./styles/global.css";
 import "./styles/animation.css";
@@ -16,35 +16,27 @@ import "./styles/services.css";
 import "./styles/advantages.css";
 import "./styles/workprocess.css";
 import "./styles/reviews.css";
-
+import "./styles/faq.css";
+import "./styles/contact.css";
+import "./styles/footer.css";
 
 function App() {
-
   return (
     <>
+      <div id="top" />
       <Header />
-
       <main>
-
         <Hero />
-
         <Services />
-
         <Advantages />
-
         <WorkProcess />
-
         <Reviews />
-
+        <FAQ />
         <Contact />
-
       </main>
-
       <Footer />
     </>
   );
-
 }
-
 
 export default App;
